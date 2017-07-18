3dd3be2ac11efbdd68e297d7069f19a43dc42c98
lynn <roselynbayocboc@gmail.com>
2017-07-18 17:48:45 +0800

    dynamic home page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   # Send email in development mode?
 config.action_mailer.perform_deliveries = true
+
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
